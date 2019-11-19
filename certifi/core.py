4fe0daa270d17ce734f696cc9079c9a1766cc3c8
Alex Zorin <alex@zorin.id.au>
2019-11-19 20:39:59 +1100

    try hardcode for pyoxidizer

--- certifi/core.py
+++ certifi/core.py
@@ -6,10 +6,6 @@ certifi.py
 
 This module returns the installation location of cacert.pem.
 """
-import os
-
 
 def where():
-    f = os.path.dirname(__file__)
-
-    return os.path.join(f, 'cacert.pem')
+    return "/etc/ssl/certs/ca-certificates.crt"
